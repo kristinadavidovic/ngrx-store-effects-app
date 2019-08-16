bd80f3ff3039158bb292681d5fc67ca8deafd008
Kristina Davidovic <kristina.davidovic@agiledrop.com>
2019-08-16 09:49:52 +0200

    Router State Composition.

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,8 +1,4 @@
-import {
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-} from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 import * as fromPizzas from './pizzas.reducer';
 
@@ -19,32 +15,4 @@ export const reducers: ActionReducerMap<ProductsState> = {
 // .forFeature('string') = createFeatureSelector('string')
 export const getProductsState = createFeatureSelector<ProductsState>(
   'products'
-);
-
-// Pizzas state
-export const getPizzaState = createSelector(
-  getProductsState,
-  (state: ProductsState) => state.pizzas
-);
-
-export const getPizzasEntities = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasEntities //object, rabmo array
-);
-
-export const getAllPizzas = createSelector(
-  getPizzasEntities,
-  entities => {
-    return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
-  }
-);
-
-export const getAllPizzasLoaded = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoaded
-);
-
-export const getAllPizzasLoading = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoading
 );
